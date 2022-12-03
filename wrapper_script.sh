17c53f84048bdf8463e176b850e3ee092048ef51
Alfie Rushby <psyar13@nottingham.ac.uk>
2022-12-03 20:50:21 +0000

    Test

--- wrapper_script.sh
+++ wrapper_script.sh
@@ -4,9 +4,9 @@ cd /opt/app-root/src/
 # Start the first process
 ./backend_process.sh &
 
-#cd ./AR-app
+cd ./AR-app
 # Start the second process
-#./frontend_process.sh
+./frontend_process.sh &
   
 # Wait for any process to exit
 wait -n
