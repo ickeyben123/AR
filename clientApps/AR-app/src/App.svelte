1fd8c429bc47ee6d5129bbf975cf46de636685d9
Alfie Rushby <psyar13@nottingham.ac.uk>
2023-02-07 16:54:45 +0000

    Fixed mongodb connection issue

--- clientApps/AR-app/src/App.svelte
+++ clientApps/AR-app/src/App.svelte
@@ -11,7 +11,7 @@
         <script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar-nft.js"></script>
     </head>
 
-    Hello.
+    <h>THis is a test.</h>
 </main>
 
 <style>
