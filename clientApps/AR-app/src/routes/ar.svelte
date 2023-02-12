1563fe33cd6350a4d7d94bdb0115d057edc4ee38
Alan-Stephen <stephenalan101@gmail.com>
2023-02-12 22:06:38 +0000

    add ar.js a-scene add a-scene tag onto ar page to try test ar functionality

---
+++ clientApps/AR-app/src/routes/ar.svelte
@@ -0,0 +1,4 @@
+<a-scene vr-mode-ui='enabled: false' arjs='sourceType: webcam; videoTexture: true; debugUIEnabled: false' renderer='antialias: true; alpha: true'>
+<a-camera gps-new-camera='gpsMinDistance: 5'></a-camera>
+<a-entity material='color: red' geometry='primitive: box' gps-new-entity-place="latitude: 52.930754; longitude: -1.208886" scale="10 10 10"></a-entity>
+</a-scene>
