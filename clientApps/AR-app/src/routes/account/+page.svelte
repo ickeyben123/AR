8a5d81d5637ffcd4137d117125d9153c6bd5187c
Alan-Stephen <stephenalan101@gmail.com>
2023-03-26 16:36:46 +0100

    add buttons for update email and password add buttons for update email and password also add placeholder code for fetch requests

--- clientApps/AR-app/src/routes/account/+page.svelte
+++ clientApps/AR-app/src/routes/account/+page.svelte
@@ -1,3 +1,55 @@
+
+
 <body>
-    hello 
+    <div class = "buttons">
+        <button on:click={sendToUpdateEmail}>
+            Update Email
+        </button>
+    </div>
+    <div class = "buttons">
+        <button on:click={sendToUpdatePasswod}>
+            Update password
+        </button>
+    </div> 
 </body>
+
+<script>   
+
+    function sendToUpdateEmail(){
+        window.location.href = "/update_email";
+    }
+
+    function sendToUpdatePasswod(){
+        window.location.href = "/update_password";
+    }
+    async function updateEmail(newEmail){
+        const response = await fetch("http://localhost:3000/user"{
+            method: 'PUT',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                "email":newEmail,
+            }
+        )
+      });
+    }
+
+    function updatePassword(newPassword){  
+        const response = await fetch("http://localhost:3000/user"{
+            method: 'PUT',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                "email":newPassword,
+            }
+        )
+      });
+    }
+</script>
+
+
+        
