913d4849accc1e0f3a51f3cf6725541ff0f8df99
Alfie Rushby <psyar13@nottingham.ac.uk>
2023-03-26 14:38:04 +0100

    Made updating the user less insecure.

--- server/routes/user-route.js
+++ server/routes/user-route.js
@@ -4,10 +4,11 @@ import * as userController from '../controllers/user-controller.js';
 import * as validation from '../middleware/validation.js';
 import * as validationJWT from '../middleware/validateJWT.js';
 
-router.post("/", [validation.checkDuplicateUsernameOrEmail,validation.validatePassword],userController.addUser);
+router.post("/", [validation.checkDuplicateEmail,validation.checkDuplicateUsername,validation.validatePassword],userController.addUser);
 router.get("/all",[validationJWT.verifyToken,validationJWT.isAdmin], userController.getUsers);
 router.get("/",[validationJWT.verifyToken], userController.getUser);
-router.put("/",[validationJWT.verifyToken], userController.updateUser);
+router.put("/email",[validationJWT.verifyToken,validation.checkDuplicateEmail], userController.updateEmail);
+router.put("/pass",[validationJWT.verifyToken,validation.validatePassword], userController.updatePassword);
 router.delete("/", [validationJWT.verifyToken,validationJWT.isAdmin], userController.deleteUser);
 router.post("/login", userController.loginUser);
 
