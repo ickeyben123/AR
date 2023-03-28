a773b94d5033dcadcbc06e3a80d513bc524d064e
psyal6 <psyal6@exmail.nottingham.ac.uk>
2023-03-28 12:18:42 +0100

    Add dropdown navigation menu consistent across all pages

--- clientApps/AR-app/src/routes/+layout.svelte
+++ clientApps/AR-app/src/routes/+layout.svelte
@@ -1,11 +1,73 @@
-<nav>
-        <a href="/tags">Tags here :OOOO</a>
-		<a href="/login">Login here for SQL injection</a>
-		<a href="/">Home</a>
-</nav>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<header>
+	<nav class="topBar">
+		<button class="toggleButton" on:click={() => navMenuActive = !navMenuActive}>
+			<div class="iconBar"></div>
+			<div class="iconBar"></div>
+			<div class="iconBar"></div>
+		</button>
+		{#if navMenuActive}
+		<div transition:slide style="background-color: white">
+				<table>
+					<tr><a class="navButton" href='/' on:click={() => navMenuActive = false}>Home</a></tr>
+					<tr><a class="navButton" href='/tags' on:click={() => navMenuActive = false}>Tags</a></tr>
+					<tr><a class="navButton" href='/login' on:click={() => navMenuActive = false}>Log In</a></tr>
+				</table>
+			</div>
+		{/if}
+	</nav>
+</header>
 
 <script>
 	import '../global.css'
+	import {slide} from 'svelte/transition';
+
+	let navMenuActive = false
 </script>
 
 <slot></slot>
+
+<style>
+	header {
+		position: -webkit-sticky;
+		position: sticky;
+		top: 0;
+		z-index: 2;
+	}
+	.topBar {
+		width: 100%;
+		background-color: rgb(219, 238, 255);
+		padding: 0;
+		height:7vh;
+		
+	}
+	table {
+		width: 100%;
+	}
+	.toggleButton {
+		height:100%;
+		outline: none;
+		border:none;
+		background-color: transparent;
+	}
+	.navButton {
+		background-color: rgb(219, 238, 255);
+		padding: 14px 16px;
+		text-decoration: none;
+		font-size: 17px;
+		display: block;
+		cursor: pointer;
+	}
+	.navButton:hover {
+		background-color: aliceblue;
+		text-decoration: none;
+		transition-duration: 0.4s;
+	}
+	.iconBar {
+		width: 35px;
+		height: 5px;
+		background-color: rgb(59, 116, 169);
+		margin: 6px 0;
+		border-radius: 2px;
+	}
+</style>
