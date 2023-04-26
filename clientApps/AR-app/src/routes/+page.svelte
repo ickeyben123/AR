71ee5379caa1a9542436cedad29f8f48ec2842b1
Alfie Rushby <psyar13@nottingham.ac.uk>
2023-04-26 10:32:18 +0100

    Modified Home page.

--- clientApps/AR-app/src/routes/+page.svelte
+++ clientApps/AR-app/src/routes/+page.svelte
@@ -1,43 +1,50 @@
 <div class = "content">   
-    <img class = "image" src="src/lib/assets/Find AR-logos_transparent.png" >
-    
-    <h3> Your Things? We Find Them. </h3>
-    
-    <br>
-    <div class="wrapper">
-        <div class="box">
-            <div class="left">
-                <video width="90%" height="90%" autoplay loop muted>
-                    <source src="src/lib/assets/home1.webm" type="video/webm" />
-                    Your browser does not support the video tag.
-                  </video>
+  <img class = "image" src="src/lib/assets/Find AR-logos_transparent.png" >
+  
+  <h3> Your Things? We Find Them. </h3>
+  
+  <br>
+  <div class="wrapper">
+      <div class="box">
+          <div class="left">
+              <video width="90%" height="90%" autoplay loop muted>
+                  <source src="src/lib/assets/home1.webm" type="video/webm" />
+                  Your browser does not support the video tag.
+                </video>
+          </div>
+            <div class="right center">
+        
+                <h1 class="long_text">VIEW YOUR TAGS</h1>
             </div>
-              <div class="right center">
-                <h1>VIEW YOUR TAGS</h1>
-              </div>
-        </div>
-        <div class="box">
+      </div>
+
+      <div class="box">
+        <div class="left">
             <video width="90%" height="90%" autoplay loop muted>
                 <source src="src/lib/assets/home2.webm" type="video/webm" />
                 Your browser does not support the video tag.
               </video>
         </div>
+          <div class="right center">
+              <h1 class="long_text">ADD YOUR TAGS</h1>
+          </div>
+    </div>
 
-      </div>
-      
+    </div>
+    
 
-    <a class="btn" href="/login">LOG IN</a>
-    <p></p>
-    <a class="btn" href="/signup">REGISTER</a>
+  <a class="btn" href="/login">LOG IN</a>
+  <p></p>
+  <a class="btn" href="/signup">REGISTER</a>
 </div>	
-   
+ 
 
 
 <style>
 
 .box {
-    width:100%;
-max-width: 500px;
+  width:100%;
+max-width: 350px;
 max-height: 300px;
 background-color: #369CE5;
 border-radius: 4px;
@@ -55,72 +62,80 @@ width: 100%;
 }
 
 .container {
-  margin: 1em;
+margin: 1em;
 }
 
 
 .top {
-  width: calc(100% - 6px);
-  min-height: 200px;
+width: calc(100% - 6px);
+min-height: 200px;
 }
 
 .left {
-  width: calc(60% - (.5em + 6px));
-  float: left;
-  height: 100%;
-  margin-top: 1em;
+width: calc(55% - (.25em));
+float: left;
+height: 100%;
+margin-top:0em;
 }
 
 .right {
-  width: calc(40% - (.5em + 6px));
-  float: right;
-  height: 100%;
-  margin-top: 1em;
+width: calc(45% - (.25em));
+float: right;
+height: 100%;
+margin-top: 0em;
 }
 
 
 .image {
-    max-width: 45%;
-    margin: auto;
-    text-align: centre;
-    background: #FFFDFA;
+  max-width: 200px;
+  margin: auto;
+  text-align: centre;
+  background: #FFFDFA;
 }
 
-.long_text{
-    width:50px;
-    position:relative;
-  top: 50%;
+.long_text {
+  width:100%;
+  height:100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: left;
 
-  transform: translateY(-50%);
+  color: #FFFDFA;
+  font-size:40px;
+right: 5px;
+margin: 0 0;
 }
 
 video {
-  
 
-    
+position: relative;
+top: 50%;
+transform: translateY(-50%);
+  
 }
 
 .title {
-    position:sticky;
-    margin: auto;
-    display: inline-block;
-    text-align: centre;
+  position:sticky;
+  margin: auto;
+  display: inline-block;
+  text-align: centre;
 }
 .btn {
-    display: inline-block;
-    margin:auto;
-    border-radius: 4px;
-    background: #369CE5;
-    color: #FFFDFA;
-    width: 50%;
-    min-width: 20px;
-    max-width: 250px;
-    font-size: larger;
-    padding: 20px 0px;
+  display: inline-block;
+  margin:auto;
+  border-radius: 4px;
+  background: #369CE5;
+  color: #FFFDFA;
+  width: 100%;
+  min-width: 20px;
+  max-width: 250px;
+  font-size: larger;
+  padding: 20px 0px;
 }
 .btn:hover {
-    background-color: #79b9e7;
-    color:white;
-    transition-duration: 0.4s;
+  background-color: #79b9e7;
+  color:white;
+  transition-duration: 0.4s;
 }
 </style>
