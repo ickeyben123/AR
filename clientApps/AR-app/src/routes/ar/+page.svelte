fabc88d4d4dd311bfadc5a5fe1cfbee10941264f
Alfie Rushby <psyar13@nottingham.ac.uk>
2023-05-03 17:14:45 +0100

    Threex addition.

--- clientApps/AR-app/src/routes/ar/+page.svelte
+++ clientApps/AR-app/src/routes/ar/+page.svelte
@@ -113,44 +113,121 @@ options = {
 
 
 function main() {
- const canvas = document.getElementById('canvas1');
+  const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(80, 2, 0.1, 50000);
+    const renderer = new THREE.WebGLRenderer({ 
+        canvas: document.querySelector('#canvas1') 
+    });
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(60, 1.33, 0.1, 10000);
-const renderer = new THREE.WebGLRenderer({canvas: canvas});
+    const geom = new THREE.BoxGeometry(20,20,20);
 
-const arjs = new THREEx.LocationBased(scene, camera);
-const cam = new THREEx.WebcamRenderer(renderer);
+    //const arjs = new THREEx.LocationBased(scene, camera);
 
-const geom = new THREE.BoxGeometry(20, 20, 20);
-const mtl = new THREE.MeshBasicMaterial({color: 0xff0000});
-const box = new THREE.Mesh(geom, mtl);
+    // You can change the minimum GPS accuracy needed to register a position - by default 1000m
+    const arjs = new THREEx.LocationBased(scene, camera, { gpsMinAccuracy: 15 } );
+    const cam = new THREEx.WebcamRenderer(renderer, '#video1');
 
-// Create the device orientation tracker
-const deviceOrientationControls = new THREEx.DeviceOrientationControls(camera);
+    const mouseStep = THREE.MathUtils.degToRad(5);
 
-// Change this to a location close to you (e.g. 0.001 degrees of latitude north of you)
-arjs.add(box, longitude, latitude+0.001); 
 
-arjs.startGps();
+    let orientationControls;
 
-requestAnimationFrame(render);
+    // Orientation controls only work on mobile device
+    if (isMobile()){   
+        orientationControls = new THREEx.DeviceOrientationControls(camera);
+    } 
 
-function render() {
-    if(canvas.width != canvas.clientWidth || canvas.height != canvas.clientHeight) {
-        renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
-        const aspect = canvas.clientWidth/canvas.clientHeight;
-        camera.aspect = aspect;
+    let fake = null;
+    let first = true;
+
+    arjs.on("gpsupdate", pos => {
+        if(first) {
+            setupObjects(longitude, latitude);
+            first = false;
+        }
+    });
+
+    arjs.on("gpserror", code => {
+        alert(`GPS error: code ${code}`);
+    });
+
+    // Uncomment to use a fake GPS location
+    //fake = { lat: 51.05, lon : -0.72 };
+    if(fake) {
+        arjs.fakeGps(fake.lon, fake.lat);
+    } else {
+        arjs.startGps();
+    } 
+
+
+    let mousedown = false, lastX = 0;
+
+    // Mouse events for testing on desktop machine
+    if(!isMobile()) {
+        window.addEventListener("mousedown", e=> {
+            mousedown = true;
+        });
+
+        window.addEventListener("mouseup", e=> {
+            mousedown = false;
+        });
+
+        window.addEventListener("mousemove", e=> {
+            if(!mousedown) return;
+            if(e.clientX < lastX) {
+                camera.rotation.y += mouseStep; 
+                if(camera.rotation.y < 0) {
+                    camera.rotation.y += 2 * Math.PI;
+                }
+            } else if (e.clientX > lastX) {
+                camera.rotation.y -= mouseStep;
+                if(camera.rotation.y > 2 * Math.PI) {
+                    camera.rotation.y -= 2 * Math.PI;
+                }
+            }
+            lastX = e.clientX;
+        });
+    }
+
+	function isMobile() {
+    	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        	// true for mobile device
+        	return true;
+    	}
+    	return false;
+	}
+
+    function render(time) {
+        resizeUpdate();
+        if(orientationControls) orientationControls.update();
+        cam.update();
+        renderer.render(scene, camera);
+        requestAnimationFrame(render);
+    }
+
+    function resizeUpdate() {
+        const canvas = renderer.domElement;
+        const width = canvas.clientWidth, height = canvas.clientHeight;
+        if(width != canvas.width || height != canvas.height) {
+            renderer.setSize(width, height, false);
+        }
+        camera.aspect = canvas.clientWidth / canvas.clientHeight;
         camera.updateProjectionMatrix();
     }
 
-    // Update the scene using the latest sensor readings
-    deviceOrientationControls.update();
+    function setupObjects(longitude, latitude) {
+        // Use position of first GPS update (fake or real)
+        const material = new THREE.MeshBasicMaterial({color: 0xff0000});
+        const material2 = new THREE.MeshBasicMaterial({color: 0xffff00});
+        const material3 = new THREE.MeshBasicMaterial({color: 0x0000ff});
+        const material4 = new THREE.MeshBasicMaterial({color: 0x00ff00});
+        arjs.add(new THREE.Mesh(geom, material), longitude, latitude + 0.001); // slightly north
+        arjs.add(new THREE.Mesh(geom, material2), longitude, latitude - 0.001); // slightly south
+        arjs.add(new THREE.Mesh(geom, material3), longitude - 0.001, latitude); // slightly west
+        arjs.add(new THREE.Mesh(geom, material4), longitude + 0.001, latitude); // slightly east
+    }
 
-    cam.update();
-    renderer.render(scene, camera);
     requestAnimationFrame(render);
-}
 }
 
 onMount(() => {
@@ -172,7 +249,8 @@ main();
   </script>
 
 
-<canvas id='canvas1' style='background-color: black; width: 100%; height: 100%'></canvas>
+<video id='video1' autoplay playsinline style='display:none'></video>
+<canvas id="canvas1" style='background-color: black; width:100%; height:100%; display:block'></canvas>
 
   <div class="content">
     <button style="
