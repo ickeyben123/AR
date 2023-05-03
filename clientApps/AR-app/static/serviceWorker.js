5861167ee8a9e98f56041ab789aa7b2a4fc45e50
Alfie Rushby <psyar13@nottingham.ac.uk>
2023-05-03 15:52:52 +0100

    Update

---
+++ clientApps/AR-app/static/serviceWorker.js
@@ -0,0 +1,21 @@
+self.addEventListener("install", (event) => {
+    console.log("Installed SW");
+  });
+  
+  self.addEventListener("activate", (event) => {
+    console.log("Activated SW");
+  });
+  
+  self.addEventListener("fetch", (event) => {
+    console.log("Fetch:", event.request);
+  });
+
+  self.addEventListener("push", (event) => {
+    // Double check the push event
+    console.log("received");
+      event.waitUntil(
+        self.registration.showNotification("Don't forget to place your tags!!", {
+          body: "Tag '" + event.data.text() + "' has not been placed again!",
+        })
+      );
+  });
