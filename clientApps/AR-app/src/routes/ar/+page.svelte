1e1167aef57dd2d29fe623514d24d8bd05a3123c
Alan-Stephen <stephenalan101@gmail.com>
2023-05-03 16:09:52 +0100

    start migration into three.js for AR.js motion detection still not implemented yet.

--- clientApps/AR-app/src/routes/ar/+page.svelte
+++ clientApps/AR-app/src/routes/ar/+page.svelte
@@ -5,128 +5,47 @@ Svelte Calls onMount -> Libraries loaded -> a-scene created
 a counter is kept of the libraries loaded so a-scene is only created
 once more all 3 libraries have been loaded in.
 -->
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<head>
-    <script 
-      src="https://aframe.io/releases/1.4.1/aframe.min.js"
-      on:load={loadComponent}></script>
-    <script 
-      type='text/javascript' src='https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js'
-      on:load={loadComponent}></script>
-
-      
-</head>
-
 <script>
-  import { onMount } from "svelte";
-  import { toast } from '@zerodevx/svelte-toast';
-  import * as THREEx from '@ar-js-org/ar.js/three.js/build/ar-threex-location-only.js'
 
-  /** @type {import('./$types').PageData} */  
-  export let data;
+import { onMount } from "svelte";
+import * as THREEx from '@ar-js-org/ar.js/three.js/build/ar-threex-location-only.js';
+import * as THREE from 'three';
 
-  let tagId;
-  let latitude;
-  let longitude;
-  let mounted = false;
-  let componentLoaded = 0;
-  let altitude = 3; 
-  let dist = 0;
+function main() {
+    const canvas = document.getElementById('canvas1');
 
-  // Once the 3 components of our program are loaded ready is set to True
-  // which will lead to AR being loaded
-  $: ready = componentLoaded == 2;
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(60, 1.33, 0.1, 10000);
+    const renderer = new THREE.WebGLRenderer({canvas: canvas});
 
-  
-  const loadComponent = () => {
-    componentLoaded = componentLoaded + 1;
-  };
+    const arjs = new THREEx.LocationBased(scene, camera);
+    const cam = new THREEx.WebcamRenderer(renderer);
 
-  async function updateLatLong(){
+    const geom = new THREE.BoxGeometry(20, 20, 20);
+    const mtl = new THREE.MeshBasicMaterial({color: 0xff0000});
+    const box = new THREE.Mesh(geom, mtl);
+    arjs.add(box, -0.72, 51.051); 
 
-    // fetch lat and long from database use tagId
-    // Get data from page.js. Handled via Svelte.
-    let item = data.tag_item
-    // for debugging.
-    console.log(item);
-    console.log(item.coords);
-    console.log(item.coords.latitude);
-    console.log(item.coords.longitude);
-    
-    item.coords.latitude = item.coords.latitude+0.0001;
-    item.coords.longitude = item.coords.longitude+0.0001;
 
-    // parse lat and long into a usable string format.
-    latitude = item.coords.latitude.toString();
-    longitude = item.coords.longitude.toString();
-  }
+    arjs.fakeGps(-0.72, 51.05);
 
-  onMount(() => {
+    requestAnimationFrame(render);
 
-    console.log("mounted")
-
-    // url params contain our tagId which we will use to get lat and long.
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    tagId = urlParams.get('tagId')
-    updateLatLong();
-    mounted=true;
-
-    id = navigator.geolocation.watchPosition(success, error, options);
-  });
-
-  async function setNotPlaced(){
-    
-    // change "placed" in database to false to repersent item being picked up.
-    const response = await fetch(window.location.origin + "/api/tag/"+tagId,{
-      method: 'PUT',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(
-        {
-          "placed":false,
+    function render() {
+        if(canvas.width != canvas.clientWidth || canvas.height != canvas.clientHeight) {
+            renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
+            const aspect = canvas.clientWidth/canvas.clientHeight;
+            camera.aspect = aspect;
+            camera.updateProjectionMatrix();
         }
-      )
-      });
+        cam.update();
+        renderer.render(scene, camera);
+        requestAnimationFrame(render);
     }
-
-  async function pickUp(){
-    await setNotPlaced()
-
-    // move page back to tags.
-    window.location.href = "/tags"
-  }
-
-  let id;
-let target;
-let options;
-
-function success(pos) {
-  const crd = pos.coords;
-
-  toast.push("Congratulations, you reached the target" + (dist));
 }
-
-function error(err) {
-  console.error(`ERROR(${err.code}): ${err.message}`);
-}
-
-target = {
-  latitude: 0,
-  longitude: 0,
-};
-
-options = {
-  enableHighAccuracy: true,
-  timeout: 5000,
-  maximumAge: 0,
-};
-
-
-
-
+  onMount(() => {
+    main();
+  });
   </script>
 
 <!-- 
@@ -140,35 +59,13 @@ once svelte has mounted and libraries have been loaded ready boolean will be
 true and a-scene will be loaded in.
 -->
 
-
-{#if ready}
-  <div class="table">
-    <a-scene vr-mode-ui='enabled: false' arjs='sourceType: webcam; videoTexture: true; debugUIEnabled: false' renderer='antialias: true; alpha: true'>
-      <!-- 
-        a-camera with gps-new-camera parameter specified creates an ar.js version of a-camera which is 
-        tracked by gps
-        gpsMinDistance is the minumum distance in metres you need to move before ar is updated
-      -->
-
-  <a-camera id='camera1' look-controls-enabled='true' arjs-device-orientation-controls='smoothingFactor: 0.1' gps-new-camera='gpsMinDistance: 1'> </a-camera>
-      <!--
-        place an entity repersenting the tag on the screen with appropriate lat and long
-      -->
-      <a-entity material='color: red' geometry='primitive: box' gps-new-entity-place="latitude: {latitude}; longitude: {longitude}" scale="10 10 10"></a-entity>
-
-    
-    </a-scene>
-    </div>
-{/if}
+<canvas id='canvas1' style='background-color: black; width: 100%; height: 100%'></canvas>
 
   <div class="content">
     <button style="
       position: relative;
       z-index: 100;
       margin-top: 0px;
-      margin-left: 0px;" 
-      on:click={pickUp}>
-      Pickup Tag
-    </button>
+      margin-left: 0px;"></button>
   </div>
 
